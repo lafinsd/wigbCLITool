aece8eac946ae257b49958b6d9e1bb9a823c048b
lafinsd <103396802+lafinsd@users.noreply.github.com>
2024-10-01 17:00:14 -0700

    Initial Commit

---
+++ wigbCLITool/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  wigbCLITool
+//
+//  Created by Larry on 10/1/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
